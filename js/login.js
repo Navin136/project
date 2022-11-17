575f9cc3ab3e3dbeeb44a5216326aa06b3b0c956
navin136 <nkwhitehat@gmail.com>
2022-11-17 11:31:40 +0530

    Login form completed

--- js/login.js
+++ js/login.js
@@ -1,11 +1,27 @@
-$("#submit").click(function(){
-  alert("hi");
-  // $.ajax({
-  //     url: "php/register.php",
-  //     type: "POST",
-  //     data: '',
-  //     success: function(phpresponse){
-  //         alert(phpresponse);
-  //     }
-  // });
+$(document).ready(function(){
+  $("#sub").click(function(){
+    function vldtlogin(){
+      let x = $("#uname").val();
+      if (x == "") {
+        alert("UserName must be filled");
+        return;
+      }
+      let pw = $("#passwd").val();
+      if (x == "") {
+        alert("Password must be filled");
+        return;
+      }
+    }
+    $.ajax({
+        url: "php/login.php",
+        type: "POST",
+        data: $("#loginform").serialize(),
+        success: function(phpresponse){
+          $("#result").html(phpresponse);
+          if(phpresponse=="Login success"){
+            window.location.href="profile.html";
+          }
+        }
+    });
+  });
 });
--- php/login.php
+++ php/login.php
@@ -1,31 +1,33 @@
 <?php
-echo "avin";
-    // $server_name = "localhost";
-    // $uname = "phpmyadmin";
-    // $pass= "/";
-    // $db_name = "phpmyadmin";
-    // $connect=new mysqli($server_name, $uname, $pass, $db_name);
+    $server_name = "localhost";
+    $uname = "phpmyadmin";
+    $pass= "/";
+    $db_name = "phpmyadmin";
+    $connect=new mysqli($server_name, $uname, $pass, $db_name);
     
-    // if ($connect->connect_error){
-    //     die("Connection failed: ");
-    // }
-    // $username = 'hello';
-    // $passwd = 'hihello';
-    // if($stmt = $connect->prepare("SELECT * FROM details where USERNAME=?")){
-    //     $stmt->bind_param("s", $username);
-    //     $stmt->execute();
-    //     $result=$stmt->get_result();
-    //     $rows=mysqli_num_rows($result);
-    //     print_r($rows);
-    // }
-    // $stmt->bind_param("ss",hello,hihello);
-    // echo "hello";
-    // if($stmt->execute()){
-    //     echo "orked"
-    // }
-    // else{
-    //     echo "bindind error";
-    // }
+    if ($connect->connect_error){
+        die("Connection failed: ");
+    }
+    $username = $_POST["username"];
+    $passwd = $_POST["passwd"];
+    $stmt = $connect->prepare("SELECT * FROM details where USERNAME=? and PASSWD=?");
+    $stmt->bind_param("ss", $username,$passwd);
+    $stmt->execute();
+    $result=$stmt->get_result();
+    $rows=mysqli_num_rows($result);
+    $canLogin="";
+    if($rows>0)
+        $canLogin=true;
+    else{
+        $canLogin=false;
+    }
+    if($canLogin){
+        echo "Login success";
+    }
+    else{
+        echo "<h2 style=\"color:green\";><center> Wrong Username or password </center></h1>";
+        echo "<h2> <center>Click <a style=\"color:blue\"; href=\"login.html\">here</a> to Try Again </center></h2>";
+    }
     // $stmt->close();
     // $connect->close();
 ?>
