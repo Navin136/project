8adc9173486ac5ed843cf7ac9b94efe6c4d9046e
navin136 <nkwhitehat@gmail.com>
2022-11-18 11:07:42 +0530

    list History of Updates

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
-    if(!localStorage.getItem("username")){
-        window.location.url="login.html";
+    if(localStorage.getItem("username")){
+        let username = localStorage.getItem("username");
+        let passwd = localStorage.getItem("passwd");
+        localStorage.setItem("dob",$("#dob").val());
+        localStorage.setItem("mobile",$("#mobile").val());
+        localStorage.setItem("state",$("#state").val());
+        let dob = localStorage.getItem("dob");
+        let mobile = localStorage.getItem("mobile");
+        let state = localStorage.getItem("state");
+        let mode = "display";
+        $.ajax({
+            url: "php/profile.php",
+            type: "POST",
+            data: {username, passwd, dob, mobile, state,mode},
+            success: function(phpresponse){
+                $("#result").html("");
+                $("#result").html(phpresponse);
+            }
+        });
     }
     $("#reset").click(function(){
         localStorage.clear();
@@ -15,12 +32,12 @@ $(document).ready(function(){
         let dob = localStorage.getItem("dob");
         let mobile = localStorage.getItem("mobile");
         let state = localStorage.getItem("state");
+        let mode = "update";
         $.ajax({
             url: "php/profile.php",
             type: "POST",
-            data: {username, passwd, dob, mobile, state},
+            data: {username, passwd, dob, mobile, state,mode},
             success: function(phpresponse){
-                $("#container").hide();
                 $("#result").html(phpresponse);
             }
         });
--- php/profile.php
+++ php/profile.php
@@ -5,13 +5,42 @@
    $dob = $_POST["dob"];
    $mobile = $_POST["mobile"];
    $state = $_POST["state"];
-   $con = new MongoDB\Client("mongodb://localhost:27017");
-   $db = $con->profile;
-   $collection = $db->data;
-   $details = (["username" => $username,"passwd" => $passwd,"dob" => $dob,"mobile" => $mobile,"state" => $state]);
-   $collection->insertOne($details);
+   $mode = $_POST["mode"];
+   if($mode == "display"){
+      require 'vendor/autoload.php';
+      $username = $_POST["username"];
+      $passwd = $_POST["passwd"];
+      $dob = $_POST["dob"];
+      $mobile = $_POST["mobile"];
+      $state = $_POST["state"];
+      $con = new MongoDB\Client("mongodb://localhost:27017");
+      $db = $con->profile;
+      $collection = $db->data;
+      $cursor = $collection->find();
+      foreach ($cursor as $details) {
+         if($details["username"] == "$username"){
+            echo "<p>"."<b>"."Welcome Back to Profile Mr.".$details["username"]."<br/>"."Your Birthday Date is ".$details["dob"]."<br/>"."Your Contact Number is ".$details["mobile"]."<br/>"."Your State is ".$details["state"]."</b>"."</p>";
+         }
+      }
+   }
+   if($mode == "update"){
+      require 'vendor/autoload.php';
+      $username = $_POST["username"];
+      $passwd = $_POST["passwd"];
+      $dob = $_POST["dob"];
+      $mobile = $_POST["mobile"];
+      $state = $_POST["state"];
+      $con = new MongoDB\Client("mongodb://localhost:27017");
+      $db = $con->profile;
+      $collection = $db->data;
+      // $collection->remove($details);
+      $details = (["username" => $username,"passwd" => $passwd,"dob" => $dob,"mobile" => $mobile,"state" => $state]);
+      $collection->insertOne($details);
+      echo "<b>Refresh page to see Update History<b>";
+   }
+   
 
-   // storing session data into redis
+   //storing session data into redis
    $redis=new Redis();
    $redis->connect('127.0.0.1',6379);
    $redis->set("username",$username);
@@ -19,5 +48,4 @@
    $redis->set("dob",$dob);
    $redis->set("mobile",$mobile);
    $redis->set("state",$state);
-   echo "<p>"."<b>"."Welcome Mr.".$redis->get("username")."<br/>"."Your Birthday Date is ".$redis->get("dob")."<br/>"."Your Contact Number is ".$redis->get("mobile")."<br/>"."Your State is ".$redis->get("state")."</b>"."</p>";
 ?>
